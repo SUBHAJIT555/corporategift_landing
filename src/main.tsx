ecbb81624169a3da4878c81f5e69649cca00f1d9
Aditya Chowdhury <adityac5102@gmail.com>
2025-11-04 12:23:30 +0530

    update: react snap

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,22 @@
-// import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
+import { createRoot, hydrateRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { StrictMode } from 'react'
+
+const rootElement = document.getElementById('root')!
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, <App />)
+} else {
+  createRoot(rootElement).render(
+
+    <StrictMode>
+      <App />
+    </StrictMode>
+
+  )
+}
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
